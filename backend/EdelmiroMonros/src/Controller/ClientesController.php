99ccf5ef12fc44130fb55abb0d8c4f69145a9db5
DANIEL ALOS PRATS <daalpr@alumnatflorida.es>
2025-02-10 13:39:53 +0100

    modificando ruta

--- backend/EdelmiroMonros/src/Controller/ClientesController.php
+++ backend/EdelmiroMonros/src/Controller/ClientesController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/clientes', name: 'app_clientes')]
 final class ClientesController extends AbstractController
 {
-    #[Route('/clientes', name: 'app_clientes')]
+    
     public function index(): Response
     {
         return $this->render('clientes/index.html.twig', [
