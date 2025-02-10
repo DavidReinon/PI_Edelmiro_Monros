22f3467a510ff75f183eb91fffafee5394ec6330
Your Name <you@example.com>
2025-02-10 13:38:46 +0100

    Añadiendo Controladores

---
+++ backend/EdelmiroMonros/src/Controller/NoticiasController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class NoticiasController extends AbstractController
+{
+    #[Route('/noticias', name: 'app_noticias')]
+    public function index(): Response
+    {
+        return $this->render('noticias/index.html.twig', [
+            'controller_name' => 'NoticiasController',
+        ]);
+    }
+}
