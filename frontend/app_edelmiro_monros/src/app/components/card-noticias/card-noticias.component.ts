2c6c89c7c396e7efb386ca7b565cae4911e4888f
daagro <daagro@alumnatflorida.es>
2025-02-11 10:29:24 +0100

    Crear Cards Noticias

---
+++ frontend/app_edelmiro_monros/src/app/components/card-noticias/card-noticias.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-noticias',
+  imports: [],
+  templateUrl: './card-noticias.component.html',
+  styleUrl: './card-noticias.component.css'
+})
+export class CardNoticiasComponent {
+
+}
