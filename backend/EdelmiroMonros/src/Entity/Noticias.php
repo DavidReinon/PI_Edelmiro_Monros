9c7df0bbc31ae1a79a666e0f236716ae2e5c0f6d
Felipe <fesalo@alumnatflorida.es>
2025-02-11 11:32:14 +0100

    ApiResource

--- backend/EdelmiroMonros/src/Entity/Noticias.php
+++ backend/EdelmiroMonros/src/Entity/Noticias.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\NoticiasRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: NoticiasRepository::class)]
+#[ApiResource]
 class Noticias
 {
     #[ORM\Id]
