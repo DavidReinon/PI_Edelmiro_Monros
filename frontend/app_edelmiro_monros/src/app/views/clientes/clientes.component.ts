195810f89459376a7134bfa975eca0b6a1bc0d93
DANIEL ALOS PRATS <daalpr@alumnatflorida.es>
2025-02-12 12:42:50 +0100

    Cliente

--- frontend/app_edelmiro_monros/src/app/views/clientes/clientes.component.ts
+++ frontend/app_edelmiro_monros/src/app/views/clientes/clientes.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CardClienteComponent } from '../../components/card-cliente/card-cliente.component';
 
 @Component({
   selector: 'app-clientes',
-  imports: [],
+  imports: [CardClienteComponent],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
 export class ClientesComponent {
+  public photo: string = '';
+  public Nombre: string = '';
+  public fecha: Date = new Date(2005, 4, 12);
+  public comentario: string = 'This is a description';
+  public valoracion: number = 5;
+
 
 }
