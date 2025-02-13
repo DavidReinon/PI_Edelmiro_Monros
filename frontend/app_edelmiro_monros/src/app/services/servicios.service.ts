6274f683f41f07450a4ce166f3bc0ea8956bfeb3
DANIEL ALOS PRATS <daalpr@alumnatflorida.es>
2025-02-13 13:10:11 +0100

    Añadiendo CRUD en Angular

--- frontend/app_edelmiro_monros/src/app/services/servicios.service.ts
+++ frontend/app_edelmiro_monros/src/app/services/servicios.service.ts
@@ -10,6 +10,19 @@ export class ServiciosService {
   constructor(public http: HttpClient) { }
   public getResponse(url: string): Observable<Response[]> {
     return this.http.get<Response[]>(url);
-    
+
     }
+    public postResponse(url: string, data: any): Observable<Response> {
+      return this.http.post<Response>(url, data);
+    }
+    public putResponse(url: string, id: number, data: any): Observable<Response> {
+      return this.http.put<Response>(`${url}/${id}`, data);
+    }  
+    public patchResponse(url: string, id: number, data: any): Observable<Response> {
+      return this.http.patch<Response>(`${url}/${id}`, data);
+    }  
+    public deleteResponse(url: string, id: number): Observable<void> {
+      return this.http.delete<void>(`${url}/${id}`);
+    }
+
 }
