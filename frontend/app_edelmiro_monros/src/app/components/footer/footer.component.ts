fd45e28d9fac721f4aa63bec5d5b49e2ceda8a28
DavidReinon <davidreinongarcia@gmail.com>
2025-02-13 10:28:22 +0100

    al acceder desde el footer a otra ruta que muestre el top de la pagina

--- frontend/app_edelmiro_monros/src/app/components/footer/footer.component.ts
+++ frontend/app_edelmiro_monros/src/app/components/footer/footer.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, ],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 }
