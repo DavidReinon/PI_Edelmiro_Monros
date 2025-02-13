ffcb3227cd9f3a90e76998a4b76e4128ffb5acc7
Felipe <fesalo@alumnatflorida.es>
2025-02-13 12:32:21 +0100

    Resolviendo conflictos

--- backend/EdelmiroMonros/src/Controller/NoticiasController.php
+++ backend/EdelmiroMonros/src/Controller/NoticiasController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Noticias;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class NoticiasController extends AbstractController
 {
@@ -14,5 +18,23 @@ final class NoticiasController extends AbstractController
         return $this->render('noticias/index.html.twig', [
             'controller_name' => 'NoticiasController',
         ]);
+    }
+
+    #[Route('/noticias', name: 'create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), associative: true);
+
+        $noticia = new Noticias();
+        $noticia->setTitulo($data['titulo']);
+        $noticia->setDescripcion($data['descripcion']);
+        $noticia->setFecha(\DateTime::createFromFormat('Y-m-d', $data['fecha']));
+        $noticia->setFoto(file_get_contents($request->files->get('foto')->getRealPath()));
+        $noticia->setUsuario($data['usuario']);
+
+        $em->persist($noticia);
+        $em->flush();
+
+        return new JsonResponse(['status' => 'Noticia creada'], status: 201);
     }
 }
