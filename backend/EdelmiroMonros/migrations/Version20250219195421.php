50d3de5996ac2e53b347791f2a60d18cc78aff29
DavidReinon <davidreinongarcia@gmail.com>
2025-02-20 00:13:14 +0100

    nueva archivo de migracion

---
+++ backend/EdelmiroMonros/migrations/Version20250219195421.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250219195421 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE noticias (id INT AUTO_INCREMENT NOT NULL, usuario_id INT DEFAULT NULL, titulo VARCHAR(255) NOT NULL, descripcion VARCHAR(255) NOT NULL, fecha DATE NOT NULL, foto LONGTEXT NOT NULL, INDEX IDX_253D925DB38439E (usuario_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE productos (id INT AUTO_INCREMENT NOT NULL, usuario_producto_id INT DEFAULT NULL, nombre VARCHAR(255) NOT NULL, descripcion VARCHAR(255) DEFAULT NULL, foto LONGTEXT DEFAULT NULL, precio DOUBLE PRECISION DEFAULT NULL, stock INT DEFAULT NULL, INDEX IDX_767490E64E6E9B00 (usuario_producto_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE resenas (id INT AUTO_INCREMENT NOT NULL, autor VARCHAR(255) NOT NULL, calificacion INT NOT NULL, fecha DATE NOT NULL, comentario VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE usuarios (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(255) NOT NULL, contraseña VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, admin TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE noticias ADD CONSTRAINT FK_253D925DB38439E FOREIGN KEY (usuario_id) REFERENCES usuarios (id)');
+        $this->addSql('ALTER TABLE productos ADD CONSTRAINT FK_767490E64E6E9B00 FOREIGN KEY (usuario_producto_id) REFERENCES usuarios (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE noticias DROP FOREIGN KEY FK_253D925DB38439E');
+        $this->addSql('ALTER TABLE productos DROP FOREIGN KEY FK_767490E64E6E9B00');
+        $this->addSql('DROP TABLE noticias');
+        $this->addSql('DROP TABLE productos');
+        $this->addSql('DROP TABLE resenas');
+        $this->addSql('DROP TABLE usuarios');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
