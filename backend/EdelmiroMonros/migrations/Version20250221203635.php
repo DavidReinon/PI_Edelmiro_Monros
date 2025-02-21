6107a320261240e4c4a224209358992fffad6cf0
Felipe Santamaría <fesalo@floridauniversitaria.es>
2025-02-21 21:37:32 +0100

    prueba9

--- backend/EdelmiroMonros/migrations/Version20250221203635.php
+++ backend/EdelmiroMonros/migrations/Version20250221203635.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250221194653 extends AbstractMigration
+final class Version20250221203635 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20250221194653 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE noticias CHANGE foto foto LONGTEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE productos CHANGE foto foto LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE usuarios CHANGE email gmail VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE noticias CHANGE foto foto LONGTEXT NOT NULL');
-        $this->addSql('ALTER TABLE productos CHANGE foto foto LONGBLOB DEFAULT NULL');
+        $this->addSql('ALTER TABLE usuarios CHANGE gmail email VARCHAR(255) NOT NULL');
     }
 }
