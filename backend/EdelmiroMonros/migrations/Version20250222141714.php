66de8627d4c505ef8083fa210324b30b9104439d
DavidReinon <davidreinongarcia@gmail.com>
2025-02-22 15:32:53 +0100

    quitrando versiones manualmente

--- backend/EdelmiroMonros/migrations/Version20250222141714.php
+++ backend/EdelmiroMonros/migrations/Version20250222141714.php
@@ -9,6 +9,7 @@ use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
+ * Version20250222141714
  */
 final class Version20250222141714 extends AbstractMigration
 {
